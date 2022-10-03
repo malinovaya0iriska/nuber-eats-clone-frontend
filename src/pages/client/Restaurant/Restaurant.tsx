48b7825c0e7fe7ace809285deff294451b3098a1
Irina Martsulevich <malinovayairiska@gmail.com>
2022-10-03 16:28:33 +0300

    add Restaurant data to its page

--- src/pages/client/Restaurant/Restaurant.tsx
+++ src/pages/client/Restaurant/Restaurant.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
 
 import {
@@ -35,5 +36,25 @@ export const Restaurant = (): ReturnComponentType => {
 
   console.log(data, params.id);
 
-  return <h1>Restaurant</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ''} | Nuber Eats</title>
+      </Helmet>
+      <div
+        className="w-full bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImage})`,
+        }}
+      >
+        <div className="bg-white w-4/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">{data?.restaurant.restaurant?.address}</h6>
+        </div>
+      </div>
+    </>
+  );
 };
